7f8cd018edc16ab86fe78cf93dd86764ed9cc745
Elyjun <julien.paulus@oclock.school>
2025-05-12 18:09:46 +0200

    correct title expansion

--- src/lib/components/SectionTitle.svelte
+++ src/lib/components/SectionTitle.svelte
@@ -4,9 +4,15 @@
 </script>
 
 {#if level === 1}
-	<h1 class="m-5 text-center lg:m-10">{title}</h1>
+	<h1 class="m-5 overflow-hidden hyphens-auto break-words text-center lg:m-10" lang="fr">
+		{title}
+	</h1>
 {:else if level === 2}
-	<h2 class="m-5 text-center lg:m-10">{title}</h2>
+	<h2 class="m-5 overflow-hidden hyphens-auto break-words text-center lg:m-10" lang="fr">
+		{title}
+	</h2>
 {:else if level === 3}
-	<h3 class="m-5 text-center lg:m-10">{title}</h3>
+	<h3 class="m-5 overflow-hidden hyphens-auto break-words text-center lg:m-10" lang="fr">
+		{title}
+	</h3>
 {/if}
